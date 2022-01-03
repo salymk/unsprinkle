5cc32dc9274b83abd79403b6beebdc4a336d0d04
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2022-01-02 20:54:53 -0700

    Solved all challenges

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,26 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+    <picture>
+      <source 
+      type="image/avif"
+      srcSet={`
+      /images/hero-img.avif 1x,
+      /images/hero-img@2x.avif 2x,
+      /images/hero-img@3x.avif 3x,
+      `}
+      />
+            <source 
+      type="image/jpeg"
+      srcSet={`
+      /images/hero-img.jpg 1x,
+      /images/hero-img@2x.jpg 2x,
+      /images/hero-img@3x.jpg 3x,
+      `}
+      />
+      <HeroImage src="/images/hero-img.jpg" alt="Large light grey cat with blue eyes"/>
+    </picture>
+      <Swoop src="/swoop.svg" alt=""/>
     </Wrapper>
   );
 };
@@ -25,6 +43,7 @@ const HeroImage = styled.img`
   width: 500px;
   height: 500px;
   max-height: 100%;
+  object-fit: cover;
 `;
 
 const Swoop = styled.img`
